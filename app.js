5b0dd9c478ff1575ba9d8ff55b5e476efbca66b6
ucurtmaavcısı <simsekmerve1717@gmail.com>
2022-02-10 16:37:43 +0300

    çalışma

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+// let button1=document.querySelector(".button1")
+// console.log(button1);
+// let button2=document.getElementsByClassName("button2")
+// console.log(button2);
+
+
+
+document.querySelector("#button1").addEventListener("click",()=>{
+   document.querySelector(".container").style.backgroundColor="blue";
+})
